d22da1b4efcd956a607c1abcce4fdca0161c6dd0
Tamilthaya <thaya2102@gmail.com>
2025-04-14 21:50:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let open = document.getElementById("open")
+let close = document.getElementById("close")
+let right = document.getElementById("right")
+
+function Slide(data){
+    if(data == "Open"){
+        right.style = "left:0";
+    }else{
+        right.style = "left:-500px"
+    }
+}
+
+let htmlcss = document.getElementById("htmlcss");
+
+function card(data){
+    switch(data){
+        case 'htmlcss':
+    }
+}
